301e3ee3395ad852f7cbc9d99cf2a84496b8b573
valentinasemina <valyaseminav@gmail.com>
2020-12-04 23:20:51 +0300

    Added calendar

---
+++ Kalendar/Kalendar/Date.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalendar
+{
+    class Date
+    {
+        public int dayM, monthM, yearM;
+        public Date()
+        {
+            dayM = DateTime.Now.Day;
+            monthM = DateTime.Now.Month;
+            yearM = DateTime.Now.Year;
+        }
+        public void NextDate()
+        {
+            dayM++;
+            int[] md = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if ((yearM % 4 == 0 && yearM % 100 != 0) || (yearM % 4 == 0 && yearM % 100 == 0 && yearM % 400 == 0) )
+            {
+                md[1] = 29;
+            }
+
+            if (monthM==12&&dayM == md[monthM-1]+1)
+            {
+                yearM++;
+                monthM = 1;
+                dayM = 1;
+            }
+            if ( dayM == md[monthM - 1]+1)
+            {
+                dayM = 1;
+                monthM++;
+            }
+           
+        }
+ 
+    }
+}
---
+++ Kalendar/Kalendar/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            Date date = new Date();
+            tb_day.Text = date.dayM.ToString(); 
+            tb_month.Text = date.monthM.ToString(); 
+            tb_year.Text = date.yearM.ToString(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Date date = new Date();
+            date.dayM = Convert.ToInt32(tb_day.Text);
+            date.monthM = Convert.ToInt32(tb_month.Text);
+            date.yearM = Convert.ToInt32(tb_year.Text);
+
+            if (Convert.ToInt32(tb_day.Text) > 31 || Convert.ToInt32(tb_day.Text) < 0 || (Convert.ToInt32(tb_day.Text) > 29 && Convert.ToInt32(tb_month.Text) == 2) ||
+                Convert.ToInt32(tb_month.Text) > 12 || Convert.ToInt32(tb_month.Text) < 0|| 
+                (Convert.ToInt32(tb_month.Text) ==2 && Convert.ToInt32(tb_day.Text) == 29 &&!((Convert.ToInt32(tb_year.Text) % 4 == 0 && Convert.ToInt32(tb_year.Text) % 100 != 0) || (Convert.ToInt32(tb_year.Text) % 4 == 0 && Convert.ToInt32(tb_year.Text) % 100 == 0 && Convert.ToInt32(tb_year.Text) % 400 == 0)))||
+                (Convert.ToInt32(tb_day.Text) == 31 && (Convert.ToInt32(tb_month.Text)==4 || Convert.ToInt32(tb_month.Text) == 6 || Convert.ToInt32(tb_month.Text) == 9 || Convert.ToInt32(tb_month.Text) == 11)))
+            {
+                tb_day.Clear();
+                tb_month.Clear();
+                tb_year.Clear();
+                MessageBox.Show("Неправильная дата");
+            }
+            else
+            {
+                date.NextDate();
+                tb_day.Text = date.dayM.ToString();
+                tb_month.Text = date.monthM.ToString();
+                tb_year.Text = date.yearM.ToString();
+            }
+        }
+    }
+}
