38355ae71f2e4f5bfe25a6fa5759524df6e71311
valentinasemina <valyaseminav@gmail.com>
2020-12-11 17:05:58 +0300

    Create calendare

---
+++ Kalendar/Kalendar/Date.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalendar
+{
+    class Date
+    {
+
+        bool isVisocos(int year)
+        {
+            if (!((yearM % 4 == 0 && yearM % 100 != 0) || yearM % 4 == 0 && yearM % 400 == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int dayM, monthM, yearM;
+        public Date()
+        {
+            dayM = DateTime.Now.Day;
+            monthM = DateTime.Now.Month;
+            yearM = DateTime.Now.Year;
+        }
+        public void NextDate()
+        {
+            dayM++;
+            int[] md = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if ((yearM % 4 == 0 && yearM % 100 != 0) || (yearM % 4 == 0 && yearM % 400 == 0) )
+            {
+                md[1] = 29;
+            }
+
+            if (monthM>=12&&dayM >= md[monthM-1]+1)
+            {
+                yearM++;
+                monthM = 1;
+                dayM = 1;
+            }
+            if ( dayM >= md[monthM - 1]+1)
+            {
+                dayM = 1;
+                monthM++;
+            }
+           
+        }
+       public bool Check()
+       {
+            int day = 0;
+              if (!Int32.TryParse(dayM.ToString(), out day))
+                {
+                    MessageBox.Show("Дата не содержит букв");
+                return false;
+                }
+                if (dayM > 31 || dayM < 0)
+                {
+                 MessageBox.Show("Введите число от 1 до 31");
+                return false;
+            }
+             int month = 0;
+                if (!Int32.TryParse(monthM.ToString(), out month))
+                {
+                MessageBox.Show ( "Дата не содержит букв");
+                return false;
+            }
+                if (monthM > 12 || monthM< 0)
+                {
+                    MessageBox.Show("Введите число от 1 до 12");
+                return false;
+            }
+                if (dayM == 31 && (monthM == 4 || dayM == 6 || monthM == 9 || monthM == 11))
+                {
+                    MessageBox.Show ("В этом месяце не 31 день");
+                return false;
+            }
+                if (monthM == 2 && dayM == 29 && isVisocos(yearM))
+                {
+                 MessageBox.Show("В этом месяце не 29 дней");
+                return false;
+            }
+            return true;
+       }
+ 
+    }
+}
---
+++ Kalendar/Kalendar/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalendar
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Use_param_param_pum(Date date, TextBox tb_day, TextBox tb_month, TextBox tb_year)
+        {
+            tb_day.Text = date.dayM.ToString();
+            tb_month.Text = date.monthM.ToString();
+            tb_year.Text = date.yearM.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            Date date = new Date();
+            Use_param_param_pum(date, tb_day, tb_month, tb_year);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Date date = new Date();
+            date.dayM = Convert.ToInt32(tb_day.Text);
+            date.monthM = Convert.ToInt32(tb_month.Text);
+            date.yearM = Convert.ToInt32(tb_year.Text);
+
+           
+                date.NextDate();
+                Use_param_param_pum(date, tb_day, tb_month, tb_year);
+               // tb_day.Text = date.dayM.ToString();
+               // tb_month.Text = date.monthM.ToString();
+               // tb_year.Text = date.yearM.ToString();     
+            
+        }
+
+        private void bt_create_Click(object sender, EventArgs e)
+        {
+            Date date = new Date();
+            date.dayM = Convert.ToInt32(tb_day.Text);
+            date.monthM = Convert.ToInt32(tb_month.Text);
+            date.yearM = Convert.ToInt32(tb_year.Text);
+
+            if (!date.Check())
+            {
+                tb_day.Clear();
+                tb_month.Clear();
+                tb_year.Clear();
+            }
+        }
+    }
+}
